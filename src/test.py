f5835cb29e7adaa371c4f2c4a5b10189a9a0c209
sawal <sawalmaskey@hotmail.com>
2018-03-19 11:07:11 -0500

    Added mentions, url extraction in each tweet.

--- src/test.py
+++ src/test.py
@@ -3,5 +3,11 @@ from tweet import Tweet
 with open('sample_tweets.txt','r',encoding='utf8') as f:
     for t in f.readlines():
         tweet = Tweet(t)
-        print(vars(tweet))
+        print('Actual Tweet:\t', t)
+        print('Hashtags:\t', tweet.hashtags)
+        print('Emojis:\t\t', tweet.emoji)
+        print('Mentions:\t', tweet.mentions)
+        print('URL:\t\t', tweet.url)
+        print('Text only:\t',tweet.text)
+        print("="*75)
     
--- src/tweet.py
+++ src/tweet.py
@@ -1,15 +1,20 @@
 import emoji, re
+from nltk.tokenize import TweetTokenizer
 
 class Tweet:
     def __init__(self, tweet_str):
+        tknzr = TweetTokenizer()
         self.tweet = tweet_str
+        tokens = tknzr.tokenize(self.tweet)
         
-        self.emoji =  [c for c in self.tweet if c in emoji.UNICODE_EMOJI]
-        self.hashtags = set(part[1:] for part in self.tweet.split() if part.startswith('#'))
-        self.text = self.tweet
-        for c in self.emoji:
-            self.text = self.text.replace(c,"")
-            
-        for c in self.hashtags:
-            self.text = self.text.replace("#"+c,"")
+        self.emoji =  [c for c in tokens if c in emoji.UNICODE_EMOJI]
+        self.hashtags = set(c for c in tokens if c.startswith('#'))
+        self.mentions = set(c for c in tokens if c.startswith('@'))
+        url_regex = re.compile(r'(?:http|ftp|https)://(?:[\w_-]+(?:(?:\.[\w_-]+)+))(?:[\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?')
+        self.url = set(url_regex.findall(tweet_str))
+        
+        not_text = set(self.emoji).union(self.hashtags).union(self.mentions).union(self.url)
+        not_text.add('RT')
+        self.text = [c for c in tokens if c not in not_text]
+        
         
