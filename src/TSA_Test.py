e8661a99be54091c8f212d92f1a33ffa8a9a220e
sawal <sawalmaskey@hotmail.com>
2018-03-29 11:30:44 -0500

    Added new methods to get sentiment of a tweet.

--- src/TSA_Test.py
+++ src/TSA_Test.py
@@ -1,3 +1,26 @@
+import json
+from collections import defaultdict
+from tweet import Tweet
+from nltk.corpus import stopwords
+
 class TSA_Test:
     def __init__(self, output_file, **kwargs):
+        self.word_sentiment = defaultdict(lambda: x, json.load(open(output_file)))
+        #Add stemming option
         
+    def getSentiment(self, tweet):
+        tw = Tweet(tweet)
+        stop_words = set(stopwords.words('english'))
+        tokens = [w for w in tw.text + tw.emoji if not w in stop_words]
+        
+        sentiment = 0
+        for t in tokens:
+            sentiment += self.word_sentiment[t]
+        return sentiment/len(tokens)
+        
+        
+        
+        
+        
+        
+            
